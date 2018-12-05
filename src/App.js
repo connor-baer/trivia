f85e8ece86f54c96433f8a5ba911194e62f82136
Connor Bär <connor.baer@sumup.com>
2018-12-05 16:02:59 +0000

    Bare business logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'react-emotion/macro';
 import { css } from 'emotion/macro';
 import { ThemeProvider } from 'emotion-theming';
@@ -11,6 +11,7 @@ import {
   injectGlobalStyles
 } from '@sumup/circuit-ui';
 import { ReactComponent as LogoIcon } from './assets/logo.svg';
+import Data from './input.json';
 
 const { circuit } = themes;
 
@@ -49,48 +50,52 @@ const Container = styled('header')`
   margin: 0 auto;
 `;
 
-const App = () => (
-  <ThemeProvider theme={circuit}>
-    <Container>
-      <Logo data-testid="sumup-logo" />
-      <Card>
-        <Heading size={Heading.KILO}>Welcome to SumUp React</Heading>
-        <Text>
-          This is a <code>create-react-app</code>
-          -based project featuring some SumUp-specific customizations:
-        </Text>
-        <List size={List.MEGA}>
-          <li>Circuit UI integration</li>
-          <li>Emotion support (incl. babel plugin)</li>
-          <li>Lodash support (incl. babel plugin)</li>
-          <li>
-            SumUp ESLint and Prettier configuration (in and outside{' '}
-            <code>react-scripts</code>)
-          </li>
-        </List>
-        <Text
-          size={Text.GIGA}
-          className={css`
-            text-align: center;
-          `}
-        >
-          Now go and build things!
-          <br />
-        </Text>
-        <Text
-          size={Text.GIGA}
-          className={css`
-            text-align: center;
-            transform: scale3d(1.5, 1.5, 1);
-          `}
-        >
-          <span role="img" aria-label="Emojis for building things">
-            🔨👩🏽‍💻👨🏼‍💻🚀
-          </span>
-        </Text>
-      </Card>
-    </Container>
-  </ThemeProvider>
-);
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={circuit}>
+        <Container>
+          <Logo data-testid="sumup-logo" />
+          <Card>
+            <Heading size={Heading.KILO}>Welcome to SumUp React</Heading>
+            <Text>
+              This is a <code>create-react-app</code>
+              -based project featuring some SumUp-specific customizations:
+            </Text>
+            <List size={List.MEGA}>
+              <li>Circuit UI integration</li>
+              <li>Emotion support (incl. babel plugin)</li>
+              <li>Lodash support (incl. babel plugin)</li>
+              <li>
+                SumUp ESLint and Prettier configuration (in and outside{' '}
+                <code>react-scripts</code>)
+              </li>
+            </List>
+            <Text
+              size={Text.GIGA}
+              className={css`
+                text-align: center;
+              `}
+            >
+              Now go and build things!
+              <br />
+            </Text>
+            <Text
+              size={Text.GIGA}
+              className={css`
+                text-align: center;
+                transform: scale3d(1.5, 1.5, 1);
+              `}
+            >
+              <span role="img" aria-label="Emojis for building things">
+                🔨👩🏽‍💻👨🏼‍💻🚀
+              </span>
+            </Text>
+          </Card>
+        </Container>
+      </ThemeProvider>
+    );
+  }
+}
 
 export default App;

5bde852c24ced65b00e5d6e7f487e05c15535295
Connor Bär <connor.baer@sumup.com>
2018-12-06 16:14:28 +0100

    You're a millionaire

--- src/pages/Finish/Finish.js
+++ src/pages/Finish/Finish.js
@@ -3,11 +3,27 @@ import PropTypes from 'prop-types';
 import { Heading, Button, Text } from '@sumup/circuit-ui';
 import styled, { css } from 'react-emotion';
 
+import questions from '../../input.json';
+
 function Finish({ navigate, score }) {
+  const isMillionaire = questions.length === score;
+
+  if (isMillionaire) {
+    return (
+      <>
+        <Heading>You're a millionare! 💰</Heading>
+        <Text>You have answered all {score} questions correctly! 🎉</Text>
+        <Button primary onClick={() => navigate('start')}>
+          Play again
+        </Button>
+      </>
+    );
+  }
+
   return (
     <>
-      <Heading>GAME OVER</Heading>
-      <Text>You have answered {score} questions correctly! 🎉</Text>
+      <Heading>GAME OVER ☠️</Heading>
+      <Text>You have answered {score} questions correctly.</Text>
       <Button primary onClick={() => navigate('start')}>
         Start Over
       </Button>

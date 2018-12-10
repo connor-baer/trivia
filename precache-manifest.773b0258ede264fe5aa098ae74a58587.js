02824097809179c8e6012326be6ef2280d3cd4db
Connor Bär <connor.baer@sumup.com>
2018-12-10 20:01:10 +0100

    Updates

---
+++ precache-manifest.773b0258ede264fe5aa098ae74a58587.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a8065b09c6974ba4e5ff44328b4dcfd",
+    "url": "/static/media/elv.0a8065b0.svg"
+  },
+  {
+    "revision": "fd29b62c7392b70a217b",
+    "url": "/static/js/main.fd29b62c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e2cc0ec9d49659dde8923767bc25379e",
+    "url": "/static/media/arrow.e2cc0ec9.svg"
+  },
+  {
+    "revision": "53c965e2db1b425d1fb93a5f749f0a71",
+    "url": "/static/media/error.53c965e2.svg"
+  },
+  {
+    "revision": "49295c58fff73387a87e4168d76e78ab",
+    "url": "/static/media/warning.49295c58.svg"
+  },
+  {
+    "revision": "8df2d1ee84cbb673c5ad87953d65c7aa",
+    "url": "/static/media/valid.8df2d1ee.svg"
+  },
+  {
+    "revision": "73abbbdfe84699d5855e8524695aa3f4",
+    "url": "/static/media/logo.73abbbdf.svg"
+  },
+  {
+    "revision": "925c236ad6289a5f46a21555e6b02ca3",
+    "url": "/static/media/arrow-right.925c236a.svg"
+  },
+  {
+    "revision": "37e4c0ac74251ddb91f898470f779b77",
+    "url": "/static/media/close.37e4c0ac.svg"
+  },
+  {
+    "revision": "8b7428e3a7e4955f17f6e1d323933006",
+    "url": "/static/media/close-icon.8b7428e3.svg"
+  },
+  {
+    "revision": "4040164541c7401994646553fa3dca9b",
+    "url": "/static/media/amex.40401645.svg"
+  },
+  {
+    "revision": "0a0316d54d7697cb4a44ad3ade1a0d79",
+    "url": "/static/media/apple-pay.0a0316d5.svg"
+  },
+  {
+    "revision": "dacab0c2e17db313462d10bb47d3636b",
+    "url": "/static/media/banco-estado.dacab0c2.svg"
+  },
+  {
+    "revision": "3ef2ddff4c42b479524bde2f407329bf",
+    "url": "/static/media/cash.3ef2ddff.svg"
+  },
+  {
+    "revision": "f21d7997687913aa28eff8a3d0e5abe3",
+    "url": "/static/media/dankort.f21d7997.svg"
+  },
+  {
+    "revision": "591701eef3ac22898c6841293faa4035",
+    "url": "/static/media/diners.591701ee.svg"
+  },
+  {
+    "revision": "67794ebbf1577c5f9ec02367f7be7364",
+    "url": "/static/media/discover.67794ebb.svg"
+  },
+  {
+    "revision": "afa5afa440723a7ac88971f319891bb3",
+    "url": "/static/media/ec.afa5afa4.svg"
+  },
+  {
+    "revision": "5c3f016f27afd453a4ee673c53a8af41",
+    "url": "/static/media/elo.5c3f016f.svg"
+  },
+  {
+    "revision": "7d8ad5e6997ad3a133b0ba3726c6e3b2",
+    "url": "/static/media/spinner.7d8ad5e6.svg"
+  },
+  {
+    "revision": "055d2f6fb2a014066f4a6713804761fd",
+    "url": "/static/media/google-pay.055d2f6f.svg"
+  },
+  {
+    "revision": "6cd105862088ca2e39addfaa7b8340c8",
+    "url": "/static/media/hiper.6cd10586.svg"
+  },
+  {
+    "revision": "9fd6469aabb1f141c033e640b18422be",
+    "url": "/static/media/hipercard.9fd6469a.svg"
+  },
+  {
+    "revision": "527984ca2687183c4cc4742c148c4419",
+    "url": "/static/media/jcb.527984ca.svg"
+  },
+  {
+    "revision": "f354d995b154e9e8c77e418c5d18d0bc",
+    "url": "/static/media/maestro.f354d995.svg"
+  },
+  {
+    "revision": "b8ab0cf023c695cd7e0c762dafed4c0f",
+    "url": "/static/media/mastercard.b8ab0cf0.svg"
+  },
+  {
+    "revision": "b1fb5f2bb3f6b8312935ba4d1b497002",
+    "url": "/static/media/nfc.b1fb5f2b.svg"
+  },
+  {
+    "revision": "fe2df5f950890862b99d3aa4161f08fc",
+    "url": "/static/media/union-pay.fe2df5f9.svg"
+  },
+  {
+    "revision": "29d5c19724a90a7cb3429cee3d828220",
+    "url": "/static/media/unknown.29d5c197.svg"
+  },
+  {
+    "revision": "4076090e4bb50f21fe57adfa2ae69fce",
+    "url": "/static/media/visa-electron.4076090e.svg"
+  },
+  {
+    "revision": "7c801e1e149711b2a5ec56e18f9fe7e7",
+    "url": "/static/media/visa.7c801e1e.svg"
+  },
+  {
+    "revision": "2968e882322f08c9fd711a9378bf660a",
+    "url": "/static/media/vpay.2968e882.svg"
+  },
+  {
+    "revision": "31126d32bff7b3e5115dd8ee0c871ae9",
+    "url": "/static/media/info.31126d32.svg"
+  },
+  {
+    "revision": "23b776954aff41dca91496ef8dc878d5",
+    "url": "/static/media/success.23b77695.svg"
+  },
+  {
+    "revision": "cb6b5b914561c09df6fa7230c56404ee",
+    "url": "/static/media/message-error.cb6b5b91.svg"
+  },
+  {
+    "revision": "1b9d8ef428230b09da1ae93504ccecfd",
+    "url": "/static/media/message-success.1b9d8ef4.svg"
+  },
+  {
+    "revision": "5437e5214c32dc0a8b1e3e3749edd831",
+    "url": "/static/media/search.5437e521.svg"
+  },
+  {
+    "revision": "8646ca425b91c474c270a6c83324eae5",
+    "url": "/static/media/arrows.8646ca42.svg"
+  },
+  {
+    "revision": "283f1cfc16f30340dd98",
+    "url": "/static/js/1.283f1cfc.chunk.js"
+  },
+  {
+    "revision": "284f8ba7f0eb5b0b49604ed5b7a1c2f3",
+    "url": "/index.html"
+  }
+];
